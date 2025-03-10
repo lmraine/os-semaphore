860faf30d8f3d7f4f4a0cc2c6423848bea73daba
lmraine <55120024+lmraine@users.noreply.github.com>
2025-03-10 12:29:51 -0500

    initial template and tests

---
+++ buffer_sem.py
@@ -0,0 +1,38 @@
+import threading
+import time
+import random
+
+class Buffer:
+    def __init__(self, size):
+        #REMOVE PASS AND COMPLETE THE CLASS
+        pass
+
+class Producer(threading.Thread):
+    def __init__(self, buffer, id):
+        super().__init__()
+        #COMPLETE THE CLASS
+
+    def produce(self, item):
+        #REMOVE PASS AND COMPLETE THE METHOD
+        pass
+
+    def run(self):
+        #REMOVE PASS AND COMPLETE THE METHOD
+        pass
+
+class Consumer(threading.Thread):
+    def __init__(self, buffer, id):
+        super().__init__()
+        #COMPLETE THE CLASS
+
+    def consume(self):
+        #REMOVE PASS AND COMPLETE THE METHOD
+        pass
+
+    def run(self):
+        #REMOVE PASS AND COMPLETE THE METHOD
+        pass
+
+if __name__ == "__main__":
+    #REMOVE PASS AND CREATE OBJECTS HERE
+    pass
