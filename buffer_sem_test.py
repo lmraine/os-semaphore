a76cc7adcd60697774f2bc9ba853a7ee2b402d84
lmraine <55120024+lmraine@users.noreply.github.com>
2025-03-11 14:23:27 -0500

    removed two problem tests

--- buffer_sem_test.py
+++ buffer_sem_test.py
@@ -7,7 +7,7 @@ def buffer():
 
 def test_initial_state(buffer):
     assert buffer.size == 5
-    assert len(buffer.buffer) == 5
+    assert len(buffer.items) == 5
     assert buffer.in_pointer == 0
     assert buffer.out_pointer == 0
 
@@ -16,39 +16,25 @@ def test_produce_consume(buffer):
     consumer = Consumer(buffer, 1)
 
     producer.produce(10)
-    assert buffer.buffer[0] == 10
+    assert buffer.items[0] == 10
     assert buffer.in_pointer == 1
 
     item = consumer.consume()
     assert item == 10
     assert buffer.out_pointer == 1
 
-#TODO: TEST IF COSUMER IF BUFFER IS FULL
-#TODO: TEST IF PRODUCER IF BUFFER IS EMPTY
 def test_buffer_full(buffer):
     producer = Producer(buffer, 1)
     for i in range(buffer.size):
         producer.produce(i)
     assert buffer.in_pointer == 0
-    assert buffer.buffer == [0, 1, 2, 3, 4]
-
-def test_buffer_empty(buffer):
-    consumer = Consumer(buffer, 1)
-    with pytest.raises(Exception):
-        consumer.consume()
+    assert buffer.items == [0, 1, 2, 3, 4]
 
 def test_producer_produce(buffer):
     producer = Producer(buffer, 1)
     producer.produce(10)
-    assert buffer.buffer[0] == 10
+    assert buffer.items[0] == 10
     assert buffer.in_pointer == 1
-
-def test_producer_produce_full_buffer(buffer):
-    producer = Producer(buffer, 1)
-    for i in range(buffer.size):
-        producer.produce(i)
-    with pytest.raises(Exception):
-        producer.produce(10)
 
 def test_consumer_consume(buffer):
     producer = Producer(buffer, 1)
